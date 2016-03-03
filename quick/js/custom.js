ddb8d55a4dd706303fa0d73f5c95f438253ebbed
Melanie Yeche <Myeche@users.noreply.github.com>
2016-03-02 19:20:25 -0800

    add smooth scroll

--- quick/js/custom.js
+++ quick/js/custom.js
@@ -4,3 +4,19 @@ $(document).ready(function () {
 		topSpacing: 0
 	});
 });
+// smooth scroll
+$(document).ready(function () {
+	$('.scrollto a[href*=#]:not([href=#])').click(function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - 50
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
