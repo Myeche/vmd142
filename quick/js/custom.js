ab6655cedbeaa5d3deeb1637ca54cfccbb2cb6d4
Myeche <myeche@mail.ccsf.edu>
2016-03-09 20:41:03 -0800

    add mlissing script

--- quick/js/custom.js
+++ quick/js/custom.js
@@ -1,50 +1,52 @@
 // sticky navbar
 $(document).ready(function () {
-	$(".navbar").sticky({
-		topSpacing: 0
-	});
+    $(".navbar").sticky({
+        topSpacing: 0
+    });
 });
 // smooth scroll
 $(document).ready(function () {
-	$('.scrollto a[href*=#]:not([href=#])').click(function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    $('.scrollto a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top - 50
-				}, 1000);
-				return false;
-			}
-		}
-	});
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 50
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
 // scroll to top
-$(document).ready(function(){
-	
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 800) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-	
-	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-	
+$(document).ready(function () {
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 800) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
 });
 // parallax
-$(function() {
+$(function () {
     $.stellar();
 });
 // main flex slider
-$(window).load(function() {
+$(window).load(function () {
     $('.main-flex-slider').flexslider({
         slideshowSpeed: 5000,
         directionNav: false,
@@ -53,12 +55,25 @@ $(window).load(function() {
     });
 });
 // owl carousel
-$(document).ready(function() {
+$(document).ready(function () {
     $("#testi-carousel").owlCarousel({
         items: 1,
-		loop:true,
-		autoplay:true,
-		autoplayTimeout:5000,
-		autoplayHoverPause:true
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true
+    });
+});
+// counter up
+jQuery(document).ready(function ($) {
+    $('.counter').counterUp({
+        delay: 100,
+        time: 800
+    });
+});
+// magnific popup
+jQuery(document).ready(function ($) {
+    $('.show-image').magnificPopup({
+        type: 'image'
     });
 });
